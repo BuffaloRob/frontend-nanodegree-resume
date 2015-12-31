044b611a7ab9271893de0a4355f454688619477e
Rob Peckham <peckham.rob@gmail.com>
2015-12-30 22:20:56 -0600

    Add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,38 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// var name = "Rob Peckham";
+// var role = "Web Developer";
+
+// var formattedName = HTMLheaderName.replace("%data%", name);
+// var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+var bio = {
+	"name": "Rob Peckham",
+	"role": "Web Developer",
+	"contacts" : {
+		"mobile": "555-555-1234",
+		"email": "peckham.rob@gmail.com",
+		"github": "BuffaloRob",
+		"twitter": "@twitter",
+		"location": "Chicago"
+	},
+	"bioPic": "images/fry.jpg",
+	"welcomeMsg": "Thanks for checking me out",
+	"skills": ["fucking shit up", "troubleshooting", "nuke stuff"]
+};
+
+// var foramattedRole = HTMLheaderRole.replace("%data%", bio.role);
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedMobile = HTMLMobile.replace("%data%", bio.contacts.mobile);
+// var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+// var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+// var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+
+// $("#header").prepend(formattedName);
+// $("#header").append(formattedRole);
+// $("#header").append(formattedMobile);
+// $("#topContacts").prepend(formattedBioPic);
+// $("#topContacts").prepend(formattedWelcomeMsg);
+// $("#topContacts").prepend(formattedSkills);
